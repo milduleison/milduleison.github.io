9d4fb924a8649b324cbd27430a9ce62d9443bc3d
milduleison <30872202+milduleison@users.noreply.github.com>
2017-08-23 21:28:24 +0300

    10 paskaita
    java script

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("labas!");
+
+//eilutes komentaras
+
+/*bloko komentaras
+
+
+*/
+
+
+
+
+var arry = [1, "2", "trys", 4, (6+5)];
+
+
+var numeris = prompt("Ivesk kalba nuo 1 iki 6");
+
+a
